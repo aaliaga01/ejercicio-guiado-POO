5377a7149af0808855533e8e684b6d8d3a5e6c9f
aaliaga-casa <a.aliaga.val@gmail.com>
2017-05-04 19:27:23 -0300

    se empieza a pgrogramar pelea

--- assets/js/pokemon.js
+++ assets/js/pokemon.js
@@ -1,9 +1,9 @@
-function Pokemon(nombre,color,podeDeAtaque){
+function Pokemon(nombre,color,poderDeAtaque){
 	this.nombre=nombre;
 	this.color=color;
 	this.nivelDeAmistad=0;
 	this.vida = 100;
-	this.podeDeAtaque = podeDeAtaque;
+	this.poderDeAtaque = poderDeAtaque;
 
 	this.mostrarPokemon= function(){
 		return("Hola, soy: " + this.nombre + " y soy de color: " + this.color)
@@ -12,13 +12,42 @@ function Pokemon(nombre,color,podeDeAtaque){
 		this.nivelDeAmistad = this.nivelDeAmistad + valor;
 	}
 	this.atacar = function(pokemon){
-		pokemon.vida = pokemon.vida - this.podeDeAtaque;
+		pokemon.vida = pokemon.vida - this.poderDeAtaque;
 	}
 }
 
-var Pikachu = new Pokemon("Pikachu","amarillo",100);
-var Charmander = new Pokemon("Charmander","rojo",20);
+const Pikachu = new Pokemon("Pikachu","amarillo",100);
+const Charmander = new Pokemon("Charmander","rojo",20);
+
+//---------------------------------------------------------------
 
 Pikachu.atacar(Charmander);
-
 console.log(Charmander.vida);
+
+//imprimir en pantalla ----------------
+
+//mostrar player 1
+function imprimirP1(){
+    var valor1=document.getElementById("play1").value;
+    var mostrar1=document.getElementById("imp1");
+    mostrar1.innerHTML="Has elegido a " + valor1;
+}
+
+//mostrar player 2
+function imprimirP2(){
+    var valor2=document.getElementById("play2").value;
+    var mostrar2=document.getElementById("imp2");
+    mostrar2.innerHTML="Has elegido a " + valor2;
+}
+
+//boton que hace todo
+function pelear(jugador1,jugador2){
+	var seleccion1=document.getElementById("play1").value;
+	var seleccion2=document.getElementById("play2").value;
+
+	seleccion1 =
+	jugador1.atacar(jugador2);
+
+	var tablaResult=document.getElementById("resultadoPelea");
+	tablaResult.innerHTML= jugador1.name + " ataca con " + jugador1.poderDeAtaque;
+}
