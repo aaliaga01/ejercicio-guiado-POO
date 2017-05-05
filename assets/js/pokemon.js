c32857dc9d2a9a14b190dff868841d489a54aaee
aaliaga-casa <a.aliaga.val@gmail.com>
2017-05-05 02:12:01 -0300

    hice lo que pude

--- assets/js/pokemon.js
+++ assets/js/pokemon.js
@@ -74,45 +74,13 @@ function pelear(){
 	var player2=document.getElementById("play2").value;
 
 	pokeDex.forEach(function(valor){
-		if (player1==valor.nombre){
-			var selec1=pokeDex.;
-			arena.push(selec1);
+		if (player1==valor.nombre) {
+			arena.push(pokeDex[valor]);
 		}
+	});
 
-	})
-
-	/*var player1=arena[0];
-	var player2=arena[1];
-
-	player1.atacar(player2);*/
+	arena[0].atacar(arena[1]);
 
 	document.getElementById("resultadoPelea").innerHTML=arena.length;
 }
 pelear();
-
-/*function peleaPokemon(){
-        var atacante = document.getElementById('pokemon_A').value;
-        var atacado = document.getElementById('pokemon_B').value; 
-        // validar que no sea el mismo pokemon
-        if (atacante=="Pokachu" && atacado=="Pokachu"){
-            alert("No puede atacarse a sí mismo")
-            return
-
-        }
-        if (atacante=="Pocky" && atacado=="Pocky"){
-            alert("No puede atacarse a sí mismo")
-            return
-        }
-        // validar que no este vacio
-        if (atacante=="0" || atacado=="0"){
-            alert("Escoja pokemon")
-            return
-        }
-
-        var ataque = prompt('Ingrese valor de ataque');
-        var pokeObj1 = new Pokemon(atacante,'rojo', ataque);
-        var pokeObj2 = new Pokemon(atacado,'otro color', 40);
-        pokeObj1.atacar(pokeObj2);
-
-        atacante.innerHTML = atacante + " atacó a " + pokeObj2.nombre + " y " + pokeObj2.nombre + " tiene " + pokeObj2.vida + " de vida restante";
-    }*/
