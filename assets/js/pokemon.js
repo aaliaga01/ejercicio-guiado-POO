bcf8e6ac91d487cb004510d8b753d17cf19eca6b
aaliaga-casa <a.aliaga.val@gmail.com>
2017-05-05 01:52:55 -0300

    no funciona

--- assets/js/pokemon.js
+++ assets/js/pokemon.js
@@ -6,7 +6,7 @@ function Pokemon(nombre,color,poderDeAtaque){
 	this.poderDeAtaque = poderDeAtaque;
 
 	this.mostrarPokemon= function(){
-		return("Hola, soy: " + this.nombre + " y soy de color: " + this.color)
+		return("Hola, soy " + this.nombre + ", mi color es " + this.color + " y mi poder de ataque es " + this.poderDeAtaque)
 	}
 	this.aumentarAmistad = function(valor){
 		this.nivelDeAmistad = this.nivelDeAmistad + valor;
@@ -16,49 +16,103 @@ function Pokemon(nombre,color,poderDeAtaque){
 	}
 }
 
+
 const pokeDex=[];
+const arena=[];
 
-const Pikachu = new Pokemon("Pikachu","amarillo",100);
-pokedex.push(Pikachu);
-const Charmander = new Pokemon("Charmander","rojo",20);
-pokedex.push(Charmander);
-const Squartle = new Pokemon("Squartle","celeste",70);
-pokedex.push(Squartle);
-const Chanchicornio = new Pokemon("Chanchicornio","rosa",1000);
-pokedex.push(Chanchicornio);
-
+	const Pikachu = new Pokemon("Pikachu","amarillo",100);
+	pokeDex.push(Pikachu);
+	const Charmander = new Pokemon("Charmander","rojo",20);
+	pokeDex.push(Charmander);
+	const Squartle = new Pokemon("Squartle","celeste",70);
+	pokeDex.push(Squartle);
+	const Chanchicornio = new Pokemon("Chanchicornio","rosa",1000);
+	pokeDex.push(Chanchicornio);
 //---------------------------------------------------------------
 
 Pikachu.atacar(Charmander);
-document.write(Charmander.vida);
+conole.log(Charmander.vida);
 
 
 //imprimir en pantalla ----------------
+
 
 //mostrar player 1
 function imprimirP1(){
     var valor1=document.getElementById("play1").value;
     var mostrar1=document.getElementById("imp1");
-    mostrar1.innerHTML="Has elegido a " + valor1;
 
+
+    pokeDex.forEach(function(valor){
+    	if (valor1==valor.nombre){
+    		return mostrar1.innerHTML=valor.mostrarPokemon();
+    	}else{"Escoge un pokemon"}
+    	
+    })
+    
 }
+imprimirP1();
+
 
 //mostrar player 2
 function imprimirP2(){
     var valor2=document.getElementById("play2").value;
     var mostrar2=document.getElementById("imp2");
-    mostrar2.innerHTML="Has elegido a " + valor2;
+
+    pokeDex.forEach(function(valor){
+    	if (valor2==valor.nombre){
+    		return mostrar2.innerHTML=valor.mostrarPokemon();
+    	}else{"Escoge un pokemon"}
+    	
+    })
 }
+imprimirP2();
 
 
-//boton que hace todo
-/*function pelear(jugador1,jugador2){
-	var seleccion1=document.getElementById("play1").value;
-	var seleccion2=document.getElementById("play2").value;
+function pelear(){
+	var player1=document.getElementById("play1").value;
+	var player2=document.getElementById("play2").value;
 
+	pokeDex.forEach(function(valor){
+		if (player1==valor.nombre){
+			var selec1=pokeDex.;
+			arena.push(selec1);
+		}
 
-	var resultado= seleccion1.atacar(seleccion2);
+	})
 
-	var tablaResultado=document.getElementById("resultadoPelea");
-	tablaResultado.innerHTML=resultado;
-}*/
+	/*var player1=arena[0];
+	var player2=arena[1];
+
+	player1.atacar(player2);*/
+
+	document.getElementById("resultadoPelea").innerHTML=arena.length;
+}
+pelear();
+
+/*function peleaPokemon(){
+        var atacante = document.getElementById('pokemon_A').value;
+        var atacado = document.getElementById('pokemon_B').value; 
+        // validar que no sea el mismo pokemon
+        if (atacante=="Pokachu" && atacado=="Pokachu"){
+            alert("No puede atacarse a sí mismo")
+            return
+
+        }
+        if (atacante=="Pocky" && atacado=="Pocky"){
+            alert("No puede atacarse a sí mismo")
+            return
+        }
+        // validar que no este vacio
+        if (atacante=="0" || atacado=="0"){
+            alert("Escoja pokemon")
+            return
+        }
+
+        var ataque = prompt('Ingrese valor de ataque');
+        var pokeObj1 = new Pokemon(atacante,'rojo', ataque);
+        var pokeObj2 = new Pokemon(atacado,'otro color', 40);
+        pokeObj1.atacar(pokeObj2);
+
+        atacante.innerHTML = atacante + " atacó a " + pokeObj2.nombre + " y " + pokeObj2.nombre + " tiene " + pokeObj2.vida + " de vida restante";
+    }*/
